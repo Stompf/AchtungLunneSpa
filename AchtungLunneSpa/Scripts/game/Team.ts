35bb512685c11e056a9e3f10488f9cbdb902b375
Stompf <anton.lundmark@gmail.com>
2015-04-26 00:03:41 +0200

    .gitignore is now working

---
+++ AchtungLunneSpa/Scripts/game/Team.ts
@@ -0,0 +1,24 @@
+﻿module Team {
+    export function toString(team: SPATest.ServerCode.Team) {
+        switch (team) {
+            case SPATest.ServerCode.Team.RED:
+                return "Team Red";
+            case SPATest.ServerCode.Team.BLUE:
+                return "Team Blue";
+            default:
+                return "undefined";
+        }
+    }
+
+	export function serverToGameEntity(team: SPATest.ServerCode.Team) {
+		switch (team) {
+            case SPATest.ServerCode.Team.RED:
+                return GameEntites.Team.RED;
+            case SPATest.ServerCode.Team.BLUE:
+                return GameEntites.Team.BLUE;
+            default:
+                return GameEntites.Team.RED;
+        }
+	}
+}
+export = Team;
---
+++ AchtungLunneSpa/ServerCode/Player.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TypeLite;
+
+namespace SPATest.ServerCode
+{
+    [TsClass]
+    public class Player
+    {
+        [TsIgnore]
+        public string GameGroupID { get; set; }
+
+        [TsIgnore]
+        public bool IsReady { get; set; }
+
+        public string ConnectionId { get; }
+        public Vector2D Position { get; set; }
+        public string Color { get; set; }
+
+        public Team Team { get; set; }
+
+        public static Size StartSize { get { return new Size { Height = 10, Width = 10 }; } }
+
+        public float LatestFrameUpdate { get; set; }
+
+		public Player(string connectionId)
+		{
+			ConnectionId = connectionId;
+		}
+
+
+
+	}
+}
