79f9f21d77a3503157749fa36d3538a67719e29e
Stompf <anton.lundmark@gmail.com>
2015-04-25 23:59:00 +0200

    .gitignore is now working

--- AchtungLunneSpa/Scripts/Game/KeyboardGroup.js
+++
@@ -1,9 +0,0 @@
-define(["require", "exports"], function (require, exports) {
-    var KeyboardGroup;
-    (function (KeyboardGroup) {
-        KeyboardGroup[KeyboardGroup["WSAD"] = 0] = "WSAD";
-        KeyboardGroup[KeyboardGroup["Arrows"] = 1] = "Arrows";
-    })(KeyboardGroup || (KeyboardGroup = {}));
-    return KeyboardGroup;
-});
-//# sourceMappingURL=KeyboardGroup.js.map
